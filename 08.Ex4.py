8fbd4da6ce95d851de1303628082b77e70aafd43
peagarj <pedrofhcosta@gmail.com>
2026-09-18 19:31:21 -0300

    Delete 08.Ex4.py

--- 08.Ex4.py
+++
@@ -1,9 +0,0 @@
-# Solicita ao usuário que digite um número
-numero = int(input("Digite um número: "))
-
-for i in range(0, 10):
-    resultado = numero * (i + 1)
-    print(f'{numero} x {i + 1} = {resultado}')
-
-
-
